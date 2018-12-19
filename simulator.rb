5542ecbf6e959ef5cd713b95df7fbb998bf4d588
Nicolas <hnsaavedraa@unal.edu.co>
2018-12-18 19:58:30 -0500

    simulator beta

--- simulator.rb
+++ simulator.rb
@@ -17,11 +17,11 @@ def calcular_promedio()
 end
 
 
-def asignar_filas(cajas, num)
+def asignar_filas(cajas, num, filas)
   if(num > 1)
-    num.times {|i| filas[i] = fila.new(cajas[i])}
+  num.times {|i| filas[i] = Fila.new(cajas[i])}
   elsif(num == 1)
-    filas[0] = cajas
+    filas[0] = Fila.new(cajas)
   end
 end
 
@@ -48,23 +48,34 @@ numero_clientes = 0
     delta_tiempo = arg[2].to_i
     cajas = Array.new(num_cajas)
     num_cajas.times {|i| cajas[i] = Caja.new()}
-
+    asignar_filas(cajas,num_cajas, filas)
     case comando
     when "s","S","9"
         seccion = "0"
-        break
+
     when "u"
         puts "selecciono fila unica"
         puts "cajas #{num_cajas} tiempo #{tiempo} delta #{delta_tiempo}"
-        asignar_filas(cajas,num_cajas)
+        for i in 1...(tiempo+1)
+          if (i == 1 || i%3 == 0)
+            num_agregar = Random.new.rand(0..5)
+            num_agregar.times {|n| filas[0].agregar_cliente( Cliente.new())}
+          end
+          for j in 0...num_cajas
+
+            if (cajas[j].cliente_caja == nil )
+              cajas[j].cliente_caja = filas[0].cola_clientes.pop
+              puts "perro"
+            elsif (cajas[j].cliente_caja.tiempoCaja == 0)
+              cajas[j].cliente_caja = filas[0].cola_clientes.pop
+            end
+          end
+        end
 
 
     when "m"
         puts "selecciono filas multiple"
-        asignar_filas(cajas,"m")
-        for i in 0...tiempo
-          
-        end
+
     else
         puts "El comando '#{comando}' no es valido"
 
