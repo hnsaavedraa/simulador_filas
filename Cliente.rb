21878a31be533e7b729b47be0cb41be9c42a6844
yocuervoc <yocuervoc@unal.edu.co>
2019-01-28 20:13:45 -0500

    Update Cliente.rb

--- Cliente.rb
+++ Cliente.rb
@@ -3,20 +3,29 @@ class Cliente
 	attr_accessor :tiempoEspera ,:nombre, :tiempoCaja
 
 	def initialize()
+=begin
 		aleatorio = Random.new
-		@nombre = aleatorio.rand(97..122).chr	
+ 		@nombre = aleatorio.rand(97..122).chr
 		@tiempoCaja = aleatorio.rand(4..25)
+=end
+
+		random_name = %w{a b d e f g h i j k l m n o p q r s t u v w x y z}
+		n=random_name.length
+		@nombre = random_name[rand(n)]
+		@tiempoCaja = rand(4..25)
 		@tiempoEspera = 0
-	
-		@nombre = "s" if @nombre == "c" 
 	end
 
 	def disminuir_tiempo_caja()
 		@tiempoCaja -= 1
 	end
 
+#funcion duplicada
+
+=begin
 	def aumentar_tiempo_espera()
 		@tiempoEspera += 1
 	end
 
+=end
 end
